7c0f5671513cc092c50954b2ce1ea0e7b45d14b0
Efsevia makri <efsemak@gmail.com>
2025-01-12 21:49:51 +0200

    Add files via upload

---
+++ turtle_heart.py
@@ -0,0 +1,27 @@
+import turtle
+import math
+
+t = turtle.Turtle()
+t.speed(50)
+t.color("black")
+turtle.bgcolor("pink")
+
+
+def corazon(n):
+    x=15 * math.sin(n) ** 3
+    y=13 * math.cos(n) - 5 * \
+        math.cos(2*n) - 2*math.cos(3*n) - \
+        math.cos(4*n)
+    return x,y
+
+t.penup()
+for i in range(20):
+    t.goto(0,0)
+    t.pendown()
+    for n in range (0, 100, 2):
+        x,y = corazon(n/15)
+        t.goto(x*i, y*i)
+    t.penup
+
+t.hideturtle()
+turtle.done
