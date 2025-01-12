69c3a63deb3a9bc39da1c1c0b6a3d63c910ca84b
Efsevia makri <efsemak@gmail.com>
2025-01-12 22:23:40 +0200

    turtle_heart.py

--- turtle_heart.py
+++ turtle_heart.py
@@ -7,7 +7,7 @@ t.color("black")
 turtle.bgcolor("pink")
 
 
-def corazon(n):
+def heart(n):
     x=15 * math.sin(n) ** 3
     y=13 * math.cos(n) - 5 * \
         math.cos(2*n) - 2*math.cos(3*n) - \
@@ -19,7 +19,7 @@ for i in range(20):
     t.goto(0,0)
     t.pendown()
     for n in range (0, 100, 2):
-        x,y = corazon(n/15)
+        x,y = heart(n/15)
         t.goto(x*i, y*i)
     t.penup
 
